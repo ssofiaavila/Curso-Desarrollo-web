ed73d9c572c252895136020ba63a88b4518cdfa6
soso <avilasofia076@gmail.com>
2022-07-01 16:41:27 -0300

    js

---
+++ Javascript/if-while.js
@@ -0,0 +1,13 @@
+var valor= 20;
+
+if (valor > 18){
+  console.log("Cumple con la edad requerida");
+}else {
+  console.log("No cumple con la edad mínima");
+}
+
+var valor2=3;
+while (valor2 > 0){
+  console.log(valor2);
+  valor2--;
+}
