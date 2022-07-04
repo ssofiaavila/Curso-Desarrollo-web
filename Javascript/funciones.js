d401c0eb283ba30a479b5b04bd5e2bb93e8882b5
soso <avilasofia076@gmail.com>
2022-07-04 12:01:43 -0300

    js

---
+++ Javascript/funciones.js
@@ -0,0 +1,6 @@
+function wellcome(nombre){
+  var frase= "Hola " + nombre + " buen día";
+  console.log(frase);
+}
+
+wellcome("Sofia"); //puedo pasar más parámetros, igual que java
--- Javascript/if-while-for.js
+++ Javascript/if-while-for.js
@@ -1,3 +1,5 @@
+//IF
+
 var valor= 20;
 
 if (valor > 18){
@@ -6,8 +8,15 @@ if (valor > 18){
   console.log("No cumple con la edad mínima");
 }
 
+
+//WHILE
 var valor2=3;
 while (valor2 > 0){
   console.log(valor2);
   valor2--;
+}
+
+//FOR
+for (var i = 0; i < 4; i++) {
+  console.log("Valor: "+ i);
 }
